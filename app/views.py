72658f0f1825722bfeac66a7eeb80b41c669fd48
Mohammad Kamal <mohammadkamalshady@gmail.com>
2016-08-14 15:11:29 +0300

    Sign In & Sign Out

---
+++ app/views.py
@@ -0,0 +1,19 @@
+@app.route('login', methods=['GET', 'POST'])
+def login():
+    erorr = None
+    if request.method == 'POST'
+        if request.form['username'] != app.config['USERNAME']:
+            erorr = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            erorr = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('Yo were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
